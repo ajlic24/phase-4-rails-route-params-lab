943f44e931e1f3a774adc9bf51ec1ace1ca7933c
ajlic24@gmail.com <ajlic24@gmail.com>
2022-08-20 22:26:04 -0400

    commit tree 05e06cf8847aea48935fdd097d4f5e8cf314daa3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+   render json: Student.all
   end
 
+  def show
+    render json: Student.find(params[:id])
+  end
 end
+
